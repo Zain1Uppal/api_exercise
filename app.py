dd73b32535c2d745dc4e783715f9d8357073e89d
Zain Uppal <zainuppal1@gmail.com>
2021-11-23 16:13:59 +0000

    add games route and error handling 404, 500

--- app.py
+++ app.py
@@ -9,6 +9,24 @@ CORS(app)
 def entry():
     return 'Welcome to this API'
 
+@app.route('/Games', methods=['GET', 'POST'])
+def pokemon_handler():
+    if request.method == 'GET':
+        return jsonify(['Farcry 6', 'Zain']), 200
+    elif request.method == 'POST':
+        data = request.json
+        return f"{data['name']} played {data['Game']}"
+
+
+@app.errorhandler(exceptions.NotFound)
+def handle_404(err):
+    return jsonify({"message": f"{err}"}), 404
+
+@app.errorhandler(exceptions.InternalServerError)
+def handle_500(err):
+    return jsonify({"message": f"{err}"}), 500
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
