61b4b0e983e77609dd6d0a72415b7c79c4c7b1e5
Zain Uppal <zainuppal1@gmail.com>
2021-11-24 10:06:44 +0000

    add procfile and requirement

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from werkzeug import exceptions
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+mail = Mail(app) 
 CORS(app)
 Games = [{'id':1,'name':'farcry 6'}, {'id':2,'name': 'COD'}]
 
@@ -19,10 +21,14 @@ def games_handler():
         for game in Games:
             print(game['name'])
             if(game['name'] == data['name']):
-                return('Game already exists')
+                return jsonify('Game already exists'), 403
         Games.append(data)
         data['id'] = len(Games)
         resp = {'Game': Games}
+        # msg = Message(f"Game {data['name']} has been added",
+        #                 sender="zainuppal2@gmail.com",
+        #                 recipients=["zainuppal1@gmail.com"])
+        # mail.send(msg)
         return jsonify(resp), 201
 
 @app.route('/Games/<int:game_id>')
